af70d21f7edeb53854c4f108b3a6d815808e0d06
hassanelijah02 <hassanelijah02@gmail.com>
2022-09-22 09:28:55 -0700

    tasks

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,9 +7,9 @@
 #include <stdio.h>
 
 /**
- * print_buffer - Prints a content 10 bytes at a time, starting with
- *                with the position of the first byte of the line in
- *         	  hexadecimal (8 chars), starting with 0.
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ *                the byte position, then showing the hex content,
+ *                then displaying printable charcaters.
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
  */
