47119bbf53aa6a6043d18ca40ff2526880f84c76
hassanelijah02 <hassanelijah02@gmail.com>
2022-10-21 17:14:35 -0700

    tasks

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for characters that are digits
- * @c: character
+ * _isupper - uppercase letters
+ * @c: character to check
  *
  * Return: 0 or 1
  */
 
-int _isdigit(int c)
+
+int _isupper(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
